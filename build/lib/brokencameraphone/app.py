33f337d934fc46c770a5ea911c007e0c21d6e737
Zac Garby <me@zacgarby.co.uk>
2023-10-07 13:29:43 +0100

    stuff and things

---
+++ build/lib/brokencameraphone/app.py
@@ -0,0 +1,49 @@
+import os
+import brokencameraphone.lib.db as db
+
+from brokencameraphone.lib import users, lobby, game
+
+from flask import Flask, session
+from flask.helpers import url_for
+from flask.templating import render_template
+from werkzeug.utils import redirect
+
+app = Flask(__name__)
+
+app.config.from_mapping(
+    SECRET_KEY=os.getenv("SECRET_KEY", default="dev"),
+    DATABASE=os.path.join(app.instance_path, "bcp.sqlite"),
+    UPLOAD_FOLDER=os.path.join(app.instance_path, "photos")
+)
+
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
+
+try:
+    os.makedirs(app.config["UPLOAD_FOLDER"])
+except OSError:
+    pass
+
+db.init_app(app)
+
+users.register_routes(app)
+lobby.register_routes(app)
+game.register_routes(app)
+
+@app.route("/")
+def index():
+    if "user_id" in session:
+        games = db.query("""
+            select * from games
+            inner join participants as p on games.id = p.game_id
+            where p.user_id = ?
+                         """,
+                         [session["user_id"]])
+        
+        return render_template("index.html",
+                               games=games,
+                               user_id=session["user_id"])
+    else:
+        return redirect(url_for("login_get"))
---
+++ build/lib/brokencameraphone/lib/helpers.py
@@ -0,0 +1,78 @@
+import brokencameraphone.lib.db as db
+
+from flask import session, flash
+from flask.helpers import url_for
+from werkzeug.utils import redirect
+
+def logged_in(handler):
+    def new_handler(*args, **kw):
+        if "user_id" not in session:
+            return redirect(url_for("index"))
+
+        return handler(*args, **kw)
+    
+    new_handler.__name__ = "logged_in_handler_" + handler.__name__
+    
+    return new_handler
+
+def with_participant(param):
+    def wrapper(handler):
+        def new_handler(*args, **kw):
+            participant = db.query("""
+                select * from participants
+                inner join games on participants.game_id = games.id
+                where user_id = ? and games.join_code = ?
+                """,
+                [session["user_id"], kw["joincode"]], one=True)
+            
+            kw[param] = participant
+            
+            return handler(*args, **kw)
+        
+        new_handler.__name__ = "with_participant_handler_" + handler.__name__
+        
+        return new_handler
+    
+    return wrapper
+
+def with_game(param):
+    def wrapper(handler):
+        def new_handler(*args, **kw):
+            game = db.query("select * from games where join_code = ?",
+                            [kw["joincode"]], one=True)
+            
+            if game is None:
+                flash(f"The game {kw['joincode']} does not exist.")
+                return redirect(url_for("index"))
+
+            kw[param] = game
+
+            return handler(*args, **kw)
+        
+        new_handler.__name__ = "with_game_handler_" + handler.__name__
+
+        return new_handler
+
+    return wrapper
+
+def lobby_owner(otherwise):
+    def wrapper(handler):
+        def new_handler(*args, **kw):
+            game = db.query("select * from games where join_code = ?",
+                                [kw["joincode"]], one=True)
+            
+            if game is None:
+                flash(f"The game {kw['joincode']} does not exist.")
+                return redirect(url_for("index"))
+            
+            if game["owner_id"] != session["user_id"]: # type: ignore
+                flash(f"You must be the owner of the game to do this!")
+                return redirect(url_for("index"))
+            
+            return handler(*args, **kw)
+        
+        new_handler.__name__ = "lobby_owner_handler_" + handler.__name__
+
+        return new_handler
+    
+    return wrapper
